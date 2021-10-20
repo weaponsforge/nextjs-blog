fcb1ba3fe48b5f01226b141f4904a2cc6c723bea
weaponsforge <forgeweapons@gmail.com>
2021-10-21 00:13:02 +0800

    Update content.

--- pages/index.js
+++ pages/index.js
@@ -23,8 +23,7 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.headingMd}>
         <p>I hammer digital weapons to solve problems and automate tasks.</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          (This is a sample website)
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
